e974a95b0cbbeba288b82be60942fe497ccbce73
Sunghyoun Kim <sunghyoun.kim@mail.utoronto.ca>
2022-08-30 03:02:20 +0000

    stuff

--- z-algo.py
+++ z-algo.py
@@ -1,5 +1,63 @@
 import math
-from check import check
+
+def check(string):
+    n = len(string)
+    last = string[-1]
+    if n > 4:
+        for i in range(-1, -(int(n/2)+2), -1):
+            if i == -1:
+                pass
+            elif i == -2:
+                if last != string[-2]:
+                    pass
+                else:
+                    return False
+            else:
+                if last == string[i]: 
+                    if string[i+1:-1] != string[i+(i+2):i]:
+                        pass
+                    else:
+                        #print("repeat found")
+                        #print(string)
+                        #print(i)
+                        #print(n+i)
+                        #print(string[i+1:-1])
+                        #print(string[i+(i+2):i])
+                        return False
+        return True
+    else:
+        return string[-1] != string[-2] and string[2:] != string[:2]
+
+def showDup(string):
+    n = len(string)
+    last = string[-1]
+    if n > 4:
+        for i in range(-1, -(int(n/2)+2), -1):
+            if i == -1:
+                pass
+            elif i == -2:
+                if last != string[-2]:
+                    pass
+                else:
+                    return False
+            else:
+                if last == string[i]: 
+                    if string[i+1:-1] != string[i+(i+2):i]:
+                        pass
+                    else:
+                        print("repeat found")
+                        print(string)
+                        print(i)
+                        print(n+i)
+                        print(string[i+1:-1])
+                        print(string[i+(i+2):i])
+                        return False
+        return True
+    else:
+        return string[-1] != string[-2] and string[2:] != string[:2]
+
+
+
 def checkDup(string):
     l = len(string)
     for i in range(1,math.floor(l/2)):
@@ -14,7 +72,7 @@ def checkDuplicates(string):
     for i in range(2, l+1 ,2):
         
         if (string[l-i:l-int(i/2)] == string[l-int(i/2):l]):
-            return True
+            return Trur
     return False
 
 
@@ -49,8 +107,23 @@ def recFunc(string, symbols={'A', 'B', 'C'}, k=2):
         else:
             if check(string+symbol):
                 recFunc(string+symbol)
+
+    '''
+    if len(string) < 100:
+        print('deadEnd')
+        print(len(string))
+        print(string)
+        for symbol in symbols:
+            if symbol  == currSymbol:
+                pass
+            else:
+                showDup(string+symbol)
+    '''
     if len(string) > 980:
         print(str(len(string)) + string) 
+        showDup(string+'A')
+        showDup(string+'B')
+        showDup(string+'C')
 
 
 

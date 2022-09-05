e85b5da98d09f4df9586373346bdd747154805e5
Sunghyoun Kim <sunghyoun.kim@mail.utoronto.ca>
2022-09-05 22:52:36 +0000

    Change fn-algo

--- fn-algo.py
+++ fn-algo.py
@@ -12,9 +12,9 @@ def possibleMoves(string, symbols):
 
 def fn_algo(string, first, symbols=['A', 'B', 'C']):
     last = string[-1]
-    print(last)
+    #print(last)
     symbols.remove(last)
-    print(symbols)
+    #print(symbols)
     pm = possibleMoves(string, symbols)
     p = len(pm)
 
@@ -23,21 +23,28 @@ def fn_algo(string, first, symbols=['A', 'B', 'C']):
         exit()
     elif p == 1:
         #This is an FN
-        if pm[0] == first:
-            front = string[1:-1]
-        else:
-            front = string[:-1]
-
+        #for i in range():
+#        if pm[0] == first:
+#            front = string[1:-1]
+#        else:
+        front = string[:-1]
         symbols.remove(front[-1])
+        for i in range(len(front)):
+            if i == len(front) - 1:
+                print('SHIT!')
+            if tightCheck(string+pm[0]+front[i:]+symbols[0]):
+                return string + pm[0] + front[i:] + symbols[0]
+        print("FUCK")
+        exit()
         return string + pm[0] + front + symbols[0]
     elif p == 2:
         return string + pm[0] 
     else:
         print('Error p greater than 2')
 
-string = 'ABACABCACB'
+string = 'A'
 first = 'A'
 while True:
-    print(string)
+    print('curr' + string)
     string = fn_algo(string, first, ['A', 'B', 'C'])
 
